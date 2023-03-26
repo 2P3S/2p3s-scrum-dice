8558b55e10c6d1c78eb4340a2cb65a2df054eba6
SeungHyun Cho <c0305sh@naver.com>
2023-03-26 15:13:41 +0900

    feat: Install Prettier locally

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -13,11 +13,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <h1 className="text-3xl font-bold">
-          Hello world!
-        </h1>
+        <h1 className="text-3xl font-bold">Hello world!</h1>
         <button className="btn">Hello daisyUI</button>
       </main>
     </>
-  )
+  );
 }
