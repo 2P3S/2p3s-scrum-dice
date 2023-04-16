9d22175579d2caefe759f81afc770ff216c08b21
SeungHyun Cho <c0305sh@naver.com>
2023-04-17 00:57:12 +0900

    [#23] frontend_atoms Input 태그 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ import classNames from 'classnames';
 import { Noto_Sans_KR, Lato } from 'next/font/google';
 
 // Todo: atomic 디자인 패턴 실험중에만 하기의 컴포넌트를 추가한다.
-import Button from '@/components/atoms/button/Button';
+import { Input } from '@/components/atoms/input';
 
 const notoSansKr = Noto_Sans_KR({
   // preload: true, 기본값
@@ -29,14 +29,12 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={classNames(notoSansKr.className, lato.variable)}>
-        {/* <Button>default btn</Button>
-        <Button loading>loading btn</Button> */}
-        <h3 className="text-3xl">다국어 폰트 테스트</h3>
-        <Button>한국어 폰트 테스트</Button>
-        <Button className="font-lato">english font test</Button>
-        <h3 className="text-3xl">숫자 폰트 테스트</h3>
-        <Button className="text-lg">123</Button>
-        <Button className="text-lg font-lato">123</Button>
+        <Input />
+        <Input value="😁 게스트 이름을 입력해주세요" className="w-full" />
+        <Input disabled />
+        <Input type="email" />
+        <Input type="password" />
+        <Input label="atomic" id="atomic" />
       </main>
     </>
   );
