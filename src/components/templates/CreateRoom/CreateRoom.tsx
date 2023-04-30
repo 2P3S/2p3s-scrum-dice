8a010baa54d31c7abfc3b9622640df12aa2ed8d4
SeungHyun Cho <tmdgus2358@gmail.com>
2023-05-01 01:47:40 +0900

    [#48] feat: 기본 카드 묶음을 클릭 이벤트로 선택 가능하도록 구현

--- src/components/templates/CreateRoom/CreateRoom.tsx
+++ src/components/templates/CreateRoom/CreateRoom.tsx
@@ -15,40 +15,90 @@ type OptionCard = {
   selected: boolean;
 };
 
+type SelectableCardGroupProps = {
+  title: string;
+  cards: number[];
+  selected: boolean;
+  onCardClick: (title: string) => void;
+};
+
+// TODO: components/organism 으로 이동.
+const SelectableCardGroup = (props: SelectableCardGroupProps) => {
+  const selectedWrapperStyle = 'bg-blue-800 bg-opacity-20 shadow-md';
+
+  return (
+    <div
+      className={classnames(
+        'p-3 rounded-md grid grid-cols-4 gap-2 place-items-center hover:border hover:border-blue-700',
+        props.selected && selectedWrapperStyle,
+      )}
+      onClick={() => props.onCardClick(props.title)}
+    >
+      {props.cards.map((item, i) => (
+        <Card key={i} state={props.selected ? 'selectable' : 'openedMe'} className="!text-4xl">
+          {item}
+        </Card>
+      ))}
+    </div>
+  );
+};
+
+const fibonacciNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+const modifiedFibonacciNumbers = [0, 0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+
 export const CreateRoom = () => {
+  // TODO: global store 에서 관리.
   const [roomName, setRoomName] = useState<string>('');
+  const [cardsState, setCardsState] = useState([
+    {
+      title: 'fibonacciNumbers',
+      cards: fibonacciNumbers,
+      selected: true,
+    },
+    {
+      title: 'modifiedFibonacciNumbers',
+      cards: modifiedFibonacciNumbers,
+      selected: false,
+    },
+  ]);
   const [optionCards, setOptionCards] = useState<OptionCard[]>([
     {
       name: 'Egg Timer',
       emoji: '🐣',
-      class: 'w-20 h-24 bg-yellow-400 rounded-lg text-[64px] shadow-lg',
+      class: 'bg-yellow-400',
       selected: true,
     },
     {
       name: 'Coffee Time',
       emoji: '☕',
-      class: 'w-20 h-24 bg-stone-800 rounded-lg text-[64px] shadow-lg',
+      class: 'bg-stone-800',
       selected: true,
     },
     {
       name: 'King',
       emoji: '👑',
-      class: 'w-20 h-24 bg-emerald-700 rounded-lg text-[64px] shadow-lg',
+      class: 'bg-emerald-700',
       selected: true,
     },
     {
       name: 'Pass',
       emoji: '？',
-      class: 'w-20 h-24 bg-red-600 rounded-lg text-[64px] shadow-lg text-white font-extrabold',
+      class: 'bg-red-600 text-white font-extrabold',
       selected: true,
     },
   ]);
 
-  const fibonacciNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-  const modifiedFibonacciNumbers = [0, 0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-
   const handleRoomNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRoomName(event.target.value);
+  };
+
+  const handleCardClick = (cardTitle: string) => {
+    setCardsState(
+      cardsState.map(card => ({
+        ...card,
+        selected: card.title === cardTitle,
+      })),
+    );
   };
 
   const handleOptionCardClick = (index: number) => {
@@ -61,11 +111,15 @@ export const CreateRoom = () => {
 
   const handleCreateRoomClick = () => {
     console.log('room name:', roomName);
+    console.log(
+      'selected cards:',
+      cardsState.find(card => card.selected),
+    );
     console.log('option cards:', optionCards);
   };
 
   return (
-    <div className="px-16 py-5 space-y-10 max-w-lg">
+    <div className="px-16 py-5 space-y-10">
       <Title headingLevel="h3" emoji="🎉" className="font-bold">
         플래닝 포커 방 생성하기
       </Title>
@@ -79,24 +133,16 @@ export const CreateRoom = () => {
       <section>
         <Paragraph className="pb-2">기본 카드 묶음을 선택해주세요</Paragraph>
         <div className="flex justify-around space-x-2">
-          <div className="flex-1">
-            <div className="p-3 bg-blue-800 bg-opacity-20 rounded-md grid grid-cols-4 gap-2 place-items-center shadow-md">
-              {fibonacciNumbers.map((item, i) => (
-                <Card key={i} state="selectable" className="w-[40px] h-[35px] text-2xl">
-                  {item}
-                </Card>
-              ))}
+          {cardsState.map(card => (
+            <div className="flex-1" key={card.title}>
+              <SelectableCardGroup
+                cards={card.cards}
+                title={card.title}
+                selected={card.selected}
+                onCardClick={handleCardClick}
+              />
             </div>
-          </div>
-          <div className="flex-1">
-            <div className=" p-3 rounded-md grid grid-cols-4 gap-2 place-items-center">
-              {modifiedFibonacciNumbers.map((item, i) => (
-                <Card key={i} state="openedMe" className="w-[40px] h-[35px] text-2xl bg-slate-300 hover:bg-slate-300 ">
-                  {item}
-                </Card>
-              ))}
-            </div>
-          </div>
+          ))}
         </div>
       </section>
       <section>
@@ -104,7 +150,13 @@ export const CreateRoom = () => {
         <div className="flex justify-between items-center space-x-3">
           {optionCards.map((optionCard, i) => (
             <div key={i} className="flex flex-col justify-center items-center" onClick={() => handleOptionCardClick(i)}>
-              <button className={classnames(optionCard.class, optionCard.selected === false && 'grayscale blur-sm')}>
+              <button
+                className={classnames(
+                  'w-20 h-24 rounded-lg text-[64px] shadow-lg',
+                  optionCard.class,
+                  optionCard.selected === false && 'grayscale blur-sm',
+                )}
+              >
                 {optionCard.emoji}
               </button>
               <Paragraph size="small" className="mt-2">
