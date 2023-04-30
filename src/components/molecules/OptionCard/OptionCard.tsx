3a3c771c304abc38163c48fc9de6f41a6ea227d7
SeungHyun Cho <tmdgus2358@gmail.com>
2023-05-01 03:06:49 +0900

    [#48] refactor: CreateRoom - ui 디자인 패턴에 맞추어 atoms, molecules ,organisms 컴포넌트 나누기

---
+++ src/components/molecules/OptionCard/OptionCard.tsx
@@ -0,0 +1,31 @@
+import classnames from 'classnames';
+
+import { Paragraph } from '@/components/atoms/paragraph';
+
+export type Option = {
+  name: string;
+  emoji: string;
+  class: string;
+  selected: boolean;
+};
+
+export type OptionCardProps = {
+  option: Option;
+};
+
+export const OptionCard = ({ option }: OptionCardProps) => (
+  <>
+    <button
+      className={classnames(
+        'w-20 h-24 rounded-lg text-[64px] shadow-lg',
+        option.class,
+        option.selected === false && 'grayscale blur-sm',
+      )}
+    >
+      {option.emoji}
+    </button>
+    <Paragraph size="small" className="mt-2">
+      {option.name}
+    </Paragraph>
+  </>
+);
--- src/components/templates/CreateRoom/CreateRoom.tsx
+++ src/components/templates/CreateRoom/CreateRoom.tsx
@@ -1,100 +1,39 @@
+import { useState } from 'react';
+
 import { Button } from '@/components/atoms/Button';
-import { Card } from '@/components/atoms/Card';
 import { Title } from '@/components/atoms/Title';
 import { Input } from '@/components/atoms/input';
 import { Paragraph } from '@/components/atoms/paragraph';
 
-import { useState } from 'react';
+import { Option, OptionCard } from '@/components/molecules/OptionCard';
 
-import classnames from 'classnames';
+import { Card, CardGroup } from '@/components/organisms/CardGroup';
 
-type OptionCard = {
-  name: string;
-  emoji: string;
-  class: string;
-  selected: boolean;
-};
-
-type SelectableCardGroupProps = {
-  title: string;
-  cards: number[];
-  selected: boolean;
-  onCardClick: (title: string) => void;
-};
-
-// TODO: components/organism 으로 이동.
-const SelectableCardGroup = (props: SelectableCardGroupProps) => {
-  const selectedWrapperStyle = 'bg-blue-800 bg-opacity-20 shadow-md';
-
-  return (
-    <div
-      className={classnames(
-        'p-3 rounded-md grid grid-cols-4 gap-2 place-items-center hover:border hover:border-blue-700',
-        props.selected && selectedWrapperStyle,
-      )}
-      onClick={() => props.onCardClick(props.title)}
-    >
-      {props.cards.map((item, i) => (
-        <Card key={i} state={props.selected ? 'selectable' : 'openedMe'} className="!text-4xl">
-          {item}
-        </Card>
-      ))}
-    </div>
-  );
-};
-
-const fibonacciNumbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
-const modifiedFibonacciNumbers = [0, 0.5, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89];
+import { FIBONACCI_NUMBERS, MODIFIED_FIBONACCI_NUMBERS, OPTION_CARDS } from '@/constants/common';
 
 export const CreateRoom = () => {
-  // TODO: global store 에서 관리.
   const [roomName, setRoomName] = useState<string>('');
-  const [cardsState, setCardsState] = useState([
+  const [cards, setCards] = useState<Card[]>([
     {
-      title: 'fibonacciNumbers',
-      cards: fibonacciNumbers,
+      title: 'fibonacci',
+      cards: FIBONACCI_NUMBERS,
       selected: true,
     },
     {
-      title: 'modifiedFibonacciNumbers',
-      cards: modifiedFibonacciNumbers,
+      title: 'modifiedFibonacci',
+      cards: MODIFIED_FIBONACCI_NUMBERS,
       selected: false,
     },
   ]);
-  const [optionCards, setOptionCards] = useState<OptionCard[]>([
-    {
-      name: 'Egg Timer',
-      emoji: '🐣',
-      class: 'bg-yellow-400',
-      selected: true,
-    },
-    {
-      name: 'Coffee Time',
-      emoji: '☕',
-      class: 'bg-stone-800',
-      selected: true,
-    },
-    {
-      name: 'King',
-      emoji: '👑',
-      class: 'bg-emerald-700',
-      selected: true,
-    },
-    {
-      name: 'Pass',
-      emoji: '？',
-      class: 'bg-red-600 text-white font-extrabold',
-      selected: true,
-    },
-  ]);
+  const [options, setOptions] = useState<Option[]>(OPTION_CARDS);
 
   const handleRoomNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setRoomName(event.target.value);
   };
 
   const handleCardClick = (cardTitle: string) => {
-    setCardsState(
-      cardsState.map(card => ({
+    setCards(
+      cards.map(card => ({
         ...card,
         selected: card.title === cardTitle,
       })),
@@ -102,20 +41,16 @@ export const CreateRoom = () => {
   };
 
   const handleOptionCardClick = (index: number) => {
-    setOptionCards(
-      optionCards.map((optionCard, i) =>
-        i === index ? { ...optionCard, selected: !optionCard.selected } : optionCard,
-      ),
-    );
+    setOptions(options.map((option, i) => (i === index ? { ...option, selected: !option.selected } : option)));
   };
 
   const handleCreateRoomClick = () => {
     console.log('room name:', roomName);
     console.log(
       'selected cards:',
-      cardsState.find(card => card.selected),
+      cards.find(card => card.selected),
     );
-    console.log('option cards:', optionCards);
+    console.log('option cards:', options);
   };
 
   return (
@@ -133,14 +68,9 @@ export const CreateRoom = () => {
       <section>
         <Paragraph className="pb-2">기본 카드 묶음을 선택해주세요</Paragraph>
         <div className="flex justify-around space-x-2">
-          {cardsState.map(card => (
+          {cards.map(card => (
             <div className="flex-1" key={card.title}>
-              <SelectableCardGroup
-                cards={card.cards}
-                title={card.title}
-                selected={card.selected}
-                onCardClick={handleCardClick}
-              />
+              <CardGroup cards={card.cards} title={card.title} selected={card.selected} onCardClick={handleCardClick} />
             </div>
           ))}
         </div>
@@ -148,20 +78,13 @@ export const CreateRoom = () => {
       <section>
         <Paragraph className="pb-2">옵션 카드를 사용해봐요</Paragraph>
         <div className="flex justify-between items-center space-x-3">
-          {optionCards.map((optionCard, i) => (
-            <div key={i} className="flex flex-col justify-center items-center" onClick={() => handleOptionCardClick(i)}>
-              <button
-                className={classnames(
-                  'w-20 h-24 rounded-lg text-[64px] shadow-lg',
-                  optionCard.class,
-                  optionCard.selected === false && 'grayscale blur-sm',
-                )}
-              >
-                {optionCard.emoji}
-              </button>
-              <Paragraph size="small" className="mt-2">
-                {optionCard.name}
-              </Paragraph>
+          {options.map((option, i) => (
+            <div
+              key={option.name}
+              className="flex flex-col justify-center items-center"
+              onClick={() => handleOptionCardClick(i)}
+            >
+              <OptionCard option={option} />
             </div>
           ))}
         </div>
