8215ec985007df04724c32814b02f813d7b3b321
SeungHyun Cho <tmdgus2358@gmail.com>
2023-07-09 20:12:16 +0900

    refactor: Card 컴포넌트에 cost & not-cost 스타일링 추가

--- src/components/atoms/card/Card.tsx
+++ src/components/atoms/card/Card.tsx
@@ -1,34 +1,66 @@
 import { ButtonHTMLAttributes } from 'react';
+
+import { OPTION_CARDS } from '@/constants/common';
 import classnames from 'classnames';
 
-type CardState = 'openedMe' | 'openedOther' | 'openedBefore' | 'selectable' | 'selected';
+const getCardStyle = (type: CardType, status: boolean, content: CardContent, vote?: Vote): string => {
+  let cardStyle = 'btn no-animation w-20 h-[92px] font-lato';
+  const optionCard = OPTION_CARDS.find(optionCard => optionCard.name === content);
 
-const cardClassNames: Record<CardState, string> = {
-  selected: 'text-5xl border-0 bg-blue-900 hover:bg-blue-900 -translate-y-4',
-  selectable: 'text-5xl border-0 bg-blue-500 hover:bg-blue-900 hover:-translate-y-4',
-  openedMe: 'text-6xl border-0 bg-slate-400 hover:bg-slate-400 hover:cursor-default',
-  openedOther:
-    'text-6xl border-slate-400 text-black bg-white hover:bg-white hover:cursor-default hover:border-slate-400',
-  openedBefore: 'btn-square loading cursor-default border-slate-400 hover:border-slate-400',
+  if (vote?.status) {
+    return `${cardStyle} text-6xl hover:cursor-default ${
+      type === 'not-cost-type' ? `${optionCard?.class} ` : 'border-slate-400 text-black bg-white hover:bg-white'
+    }`;
+  }
+
+  if (type === 'cost-type') {
+    const bgClass = status ? 'bg-blue-900' : 'bg-blue-500';
+    return `${cardStyle} text-5xl text-white border-0 ${bgClass} hover:bg-blue-900 hover:-translate-y-4`;
+  }
+
+  if (type === 'not-cost-type') {
+    const extraClass = status ? `${optionCard?.class} grayscale` : `${optionCard?.class} hover:-translate-y-4`;
+    return `${cardStyle} ${extraClass}`;
+  }
+
+  return cardStyle;
 };
 
-export type CardProps = ButtonHTMLAttributes<HTMLButtonElement> & {
-  state: CardState;
+type TempCardProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  card: Card;
 };
 
-export const Card = ({ children, className, state, ...props }: CardProps) => {
-  const cardStyle = cardClassNames[state];
-  const classNames = classnames(className, 'btn no-animation w-20 h-[92px] font-lato', cardStyle);
+const convertCardContent = (type: CardType, content: CardContent): string | number => {
+  if (type === 'cost-type') return content;
+
+  return OPTION_CARDS.find(optionCard => optionCard.name === content)?.emoji as string;
+};
+
+export const Card = ({ className, card, ...props }: TempCardProps) => {
+  const { type, status, content, vote } = card;
+  const cardStyle = getCardStyle(type, status, content, vote);
+  const convertedCardContent = convertCardContent(type, content);
 
   return (
-    <>
-      {state === 'openedBefore' ? (
-        <div className={classnames('btn no-animation w-20 h-[92px] rounded-lg text-[64px]', cardClassNames[state])} />
-      ) : (
-        <button className={classNames} {...props}>
-          {children}
-        </button>
-      )}
-    </>
+    <button className={classnames(className, cardStyle)} {...props}>
+      {convertedCardContent}
+    </button>
+  );
+};
+
+type MockCardProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  cardType: CardType;
+  status: boolean;
+  content: CardContent;
+};
+
+export const MockCard = ({ className, cardType, status, content, ...props }: MockCardProps) => {
+  const cardStyle = getCardStyle(cardType, status, content);
+  const convertedCardContent = convertCardContent(cardType, content);
+
+  return (
+    <button className={classnames(className, cardStyle)} {...props}>
+      {convertedCardContent}
+    </button>
   );
 };
