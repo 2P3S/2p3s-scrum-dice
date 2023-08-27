ea6fed03f1144d1601e32dad37d3393c7a4c6bb1
SeungHyun Cho <tmdgus2358@gmail.com>
2023-08-27 22:39:21 +0900

    feat: PokerVoting에서 평균 계산 기능 추가

--- src/components/organisms/PokerVoting/index.tsx
+++ src/components/organisms/PokerVoting/index.tsx
@@ -11,20 +11,35 @@ type PokerVotingProps = {
 export const PokerVoting = ({ room, vote }: PokerVotingProps) => {
   const member = useMemberStore(state => state.member);
 
+  // 카드 값 배열 생성
+  const cardValues = room.members
+    .filter(memberData => memberData.status) // memberData.status로 접속중인 멤버들만 필터링
+    .map(memberData => parseInt(vote.cards?.[memberData.id]?.content as string))
+    .filter(cardValue => !isNaN(cardValue));
+
+  const averageValue = cardValues.length > 0 ? cardValues.reduce((a, b) => a + b, 0) / cardValues.length : null;
+
   return (
-    <div className="flex space-x-4">
-      {room.members.map(memberData => {
-        if (!memberData.status) return null;
+    <section>
+      <div className="mb-4">
+        {averageValue !== null && <p hidden={!vote.status}>Average : {averageValue.toFixed(1)}</p>}
+      </div>
+      <div className="flex space-x-4">
+        {room.members.map(memberData => {
+          if (!memberData.status) return null;
 
-        const cardData = vote.cards?.[memberData.id];
+          const cardData = vote.cards?.[memberData.id];
 
-        return (
-          <div className="flex flex-col" key={memberData.id}>
-            {cardData ? <Card card={cardData} vote={vote} isMe={memberData.id === member?.id} /> : <TempCard />}
-            <Paragraph size="small" className="mt-2">{memberData.name}</Paragraph>
-          </div>
-        );
-      })}
-    </div>
+          return (
+            <div className="flex flex-col" key={memberData.id}>
+              {cardData ? <Card card={cardData} vote={vote} isMe={memberData.id === member?.id} /> : <TempCard />}
+              <Paragraph size="small" className="mt-2">
+                {memberData.name}
+              </Paragraph>
+            </div>
+          );
+        })}
+      </div>
+    </section>
   );
 };
