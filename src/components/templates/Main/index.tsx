21878fe95e30da9d29f43eb7d736bfd265ed10df
SeungHyun Cho <tmdgus2358@gmail.com>
2023-08-27 20:27:45 +0900

    fix: meta tag description

--- src/components/templates/Main/index.tsx
+++ src/components/templates/Main/index.tsx
@@ -1,21 +1,17 @@
 import { LinkButton } from '@/components/atoms/Button';
-import { Paragraph } from '@/components/atoms/Paragraph';
 
 export const Main = () => {
   return (
     <main className="m-auto max-w-2xl pt-32 text-center">
       <div>
-        <div className="text-2xl text-blue-600">효율적인 플래닝 포커를 위한</div>
+        <div className="text-2xl text-gray-800">효율적인 플래닝 포커를 위한</div>
         <div className="relative text-5xl font-bold text-blue-600 sm:text-5xl my-3 animate-fade animate-duration-[3000ms] animate-ease-linear">
-          Scrum Dice<span className="text-2xl ml-2">로</span>
+          Scrum Dice<span className="text-2xl font-normal text-gray-800 ml-2">에서</span>
           <span className="animate-ping absolute inline-flex h-1.5 w-1.5 rounded-full bg-blue-400 opacity-75"></span>
         </div>
-        <div className="text-2xl text-blue-600 mb-10">실시간 투표를 진행해보세요</div>
+        <div className="text-2xl text-gray-800 mb-10">실시간 투표를 진행해보세요</div>
       </div>
-      {/* <Paragraph size="small" className="">
-        팀의 플래닝 포커를 편리하고 친밀하게 만들어줄게요
-      </Paragraph> */}
-      <LinkButton href="/room" className="rounded-full w-2/4">
+      <LinkButton href="/room" className="rounded-full w-2/5">
         지금 바로 시작하기
       </LinkButton>
     </main>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,13 @@
 import { Main } from '@/components/templates/Main';
 import Head from 'next/head';
 
-import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
-import { useTranslation } from 'next-i18next';
-
 export default function Home() {
-  const translate = useTranslation(['common', 'createroom']).t;
-
   return (
     <>
       <Head>
         <title>Scrum Dice</title>
-        <meta name="description" content="지금까지 없던 혁신적인 플래닝 포커 Scrum Dice" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
+      <Main />
     </>
   );
 }
-
-export const getStaticProps = async ({ locale }: any) => ({
-  props: {
-    ...(await serverSideTranslations(locale, ['common', 'createroom'])),
-  },
-});
